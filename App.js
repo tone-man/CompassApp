7ca4066ae03ba521777457d095c0cadc58b152e9
dalymb <dalymb@merrimack.edu>
2023-05-18 15:26:24 -0400

    added QRCode

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, View, SafeAreaView, Dimensions } from 'react-native';
+export default function App() {
+  const dimwidth = Dimensions.get('window').width;
+  const dimheight = Dimensions.get('window').height;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('C:/Users/micha/OneDrive/Documents/Compass App/CompassApp/QR_Code.png')}
+        style={{ resizeMode: 'center', paddingTop: 0, width: dimwidth/5, height:dimheight/5}}
+      />
+      <SafeAreaView style={styles.safeArea}>
+        
+   </SafeAreaView>
+    </View>
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+});
