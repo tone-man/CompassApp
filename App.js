426f4acb088374e2ba581e5f5cf26e1d2997409c
toni_craveiro <theToneMan@outlook.com>
2023-05-25 15:17:16 -0400

    Demo Interface
    Made a quick Demo Interface that uses the paper library. Will Icons are not showing but this is a proof of concept.

--- App.js
+++ App.js
@@ -1,20 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import {
+  Appbar,
+  DefaultTheme,
+  Provider as PaperProvider,
+  BottomNavigation,
+} from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [index, setIndex] = React.useState(0);
+
+  const NavigationBar = () => (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+
+  const HeaderContent = () => (
+    <View style={styles.headerContent}>
+      <View style={styles.iconButtonContainer}>
+        <Icon name="user" size={24} color="black" />
+      </View>
+      <Appbar.Content
+        title={<Icon name="compass" size={24} color="black" />}
+        titleStyle={{ marginLeft: 10 }}
+        style={styles.headerTitle}
+      />
     </View>
+  );
+
+  const AnalyticsRoute = () => (
+    <View style={styles.routeContainer}>
+      <Text>Analytics Screen</Text>
+      {/* Add your analytics screen content here */}
+    </View>
+  );
+
+  const QrCodeRoute = () => (
+    <View style={styles.routeContainer}>
+      <Text>QR Code Screen</Text>
+      {/* Add your QR code screen content here */}
+    </View>
+  );
+
+  const AddContentRoute = () => (
+    <View style={styles.routeContainer}>
+      <Text>Add Content Screen</Text>
+      {/* Add your add content screen content here */}
+    </View>
+  );
+
+  const routes = [
+    { key: "analytics", title: "Analytics", icon: "chart-bar" },
+    { key: "qrCode", title: "QR Code", icon: "qrcode" },
+    { key: "addContent", title: "Add Content", icon: "plus-circle" },
+  ];
+
+  const renderScene = BottomNavigation.SceneMap({
+    analytics: AnalyticsRoute,
+    qrCode: QrCodeRoute,
+    addContent: AddContentRoute,
+  });
+
+  return (
+    <SafeAreaProvider>
+      <PaperProvider theme={DefaultTheme}>
+        <View style={styles.container}>
+          <Appbar.Header>
+            <HeaderContent />
+          </Appbar.Header>
+          <View style={styles.content}>{/* Your app content */}</View>
+          <NavigationBar />
+        </View>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+  iconButtonContainer: {
+    marginRight: 10,
+  },
+  headerTitle: {
+    flex: 1,
+    alignItems: "center",
+  },
+  routeContainer: {
+    flex: 1,
+    alignItems: "center",
   },
 });
