dfb268d072422083a5ba3883fec2e3576c5e41c9
toni_craveiro <theToneMan@outlook.com>
2023-06-05 13:20:46 -0400

    Updated Navigation Layout to Reccomendations
    React-native-navigation recommends that layouts be Stacks Nested in Tabs, so I am doing that to make our lives easier going forward.
    
    Nope, I lied, our project needs the inverse, otherwise the feel is off.

--- Navigation.js
+++ Navigation.js
@@ -1,23 +1,64 @@
 // Tabs.js
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-import Analytics from "./analytics";
-import QRCode from "./qrCode";
-import Profile from "./Profile";
 import { useNavigation } from "@react-navigation/native";
 
-import Icon from "react-native-vector-icons/Octicons";
-import Icon1 from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+const AnalyticsView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Analytics!</Text>
+    </View>
+  );
+};
+
+const QRCodeView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>QR Code!</Text>
+    </View>
+  );
+};
+
+const MasteryInputView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Mastery Input!</Text>
+    </View>
+  );
+};
+
+const ProfileView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Profile!</Text>
+    </View>
+  );
+};
+
 const ProfileIcon = ({ navigation }) => {
   return (
     <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-      <Icon1 name="person-circle-outline" size={30} color="#000" />
+      <Icon name="account-circle-outline" size={30} color="#000" />
     </TouchableOpacity>
+  );
+};
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Profile" component={ProfileView} />
+    </Stack.Navigator>
   );
 };
 
@@ -28,20 +69,30 @@ function TabNavigator() {
     <Tab.Navigator>
       <Tab.Screen
         name="Analytics"
-        component={Analytics}
+        component={AnalyticsView}
         options={{
           tabBarIcon: (props) => (
-            <Icon name="graph" size={24} color={props.color} />
+            <Icon name="google-analytics" size={24} color={props.color} />
           ),
           headerLeft: () => <ProfileIcon navigation={navigation} />,
         }}
       />
       <Tab.Screen
         name="QRCode"
-        component={QRCode}
+        component={QRCodeView}
         options={{
           tabBarIcon: (props) => (
-            <Icon1 name="ios-qr-code-outline" size={24} color={props.color} />
+            <Icon name="qrcode-scan" size={24} color={props.color} />
+          ),
+          headerLeft: () => <ProfileIcon navigation={navigation} />,
+        }}
+      />
+      <Tab.Screen
+        name="DataInput"
+        component={MasteryInputView}
+        options={{
+          tabBarIcon: (props) => (
+            <Icon name="plus-circle-outline" size={24} color={props.color} />
           ),
           headerLeft: () => <ProfileIcon navigation={navigation} />,
         }}
@@ -50,15 +101,13 @@ function TabNavigator() {
   );
 }
 
-export default function StackNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Go Back"
-        component={TabNavigator}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
-  );
-}
+export default StackNavigator;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
