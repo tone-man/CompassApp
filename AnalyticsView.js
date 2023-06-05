80d020067efc2bf34fdf8ab28f2fba270dcbc895
toni_craveiro <theToneMan@outlook.com>
2023-06-05 14:29:08 -0400

    Improved Analytics View
    Scroll is now the entire screen, table is correctly placed in scroll view.

--- AnalyticsView.js
+++ AnalyticsView.js
@@ -22,191 +22,77 @@ import {
 
 import TableExample from "./table";
 
+const ExampleGraph = () => {
+  return (
+    <LineChart
+      data={{
+        labels: ["9/1", "9/2", "9/3", "9/4", "9/5", "9/6"],
+        datasets: [
+          {
+            data: [
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+            ],
+          },
+        ],
+      }}
+      width={Dimensions.get("window").width} // from react-native
+      height={220}
+      yAxisInterval={1} // optional, defaults to 1
+      chartConfig={{
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#2F80EA",
+        backgroundGradientTo: "#2F80EA",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16,
+        },
+        propsForDots: {
+          r: "6",
+          strokeWidth: "2",
+          stroke: "#2F80EA",
+        },
+      }}
+      bezier
+      style={{
+        marginVertical: 8,
+        borderRadius: 16,
+      }}
+    />
+  );
+};
+
 const AnalyticsView = () => {
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.graphContainer}>
+      <SafeAreaView>
         <ScrollView>
-          <View style={styles.graph}>
+          <View style={styles.container}>
             <Text>Reading</Text>
-            <LineChart
-              data={{
-                labels: ["9/1", "9/2", "9/3", "9/4", "9/5", "9/6"],
-                datasets: [
-                  {
-                    data: [
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                    ],
-                  },
-                ],
-              }}
-              width={Dimensions.get("window").width} // from react-native
-              height={220}
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundColor: "#e26a00",
-                backgroundGradientFrom: "#2F80EA",
-                backgroundGradientTo: "#2F80EA",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16,
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#2F80EA",
-                },
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16,
-              }}
-            />
+            <ExampleGraph />
           </View>
-          <View style={styles.graph}>
+          <View style={styles.container}>
             <Text>Writing</Text>
-            <LineChart
-              data={{
-                labels: ["9/1", "9/2", "9/3", "9/4", "9/5", "9/6"],
-                datasets: [
-                  {
-                    data: [
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                    ],
-                  },
-                ],
-              }}
-              width={Dimensions.get("window").width} // from react-native
-              height={220}
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundColor: "#e26a00",
-                backgroundGradientFrom: "#2F80EA",
-                backgroundGradientTo: "#2F80EA",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16,
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#2F80EA",
-                },
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16,
-              }}
-            />
+            <ExampleGraph />
           </View>
-          <View style={styles.graph}>
+          <View style={styles.container}>
             <Text>Note-taking</Text>
-            <LineChart
-              data={{
-                labels: ["9/1", "9/2", "9/3", "9/4", "9/5", "9/6"],
-                datasets: [
-                  {
-                    data: [
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                    ],
-                  },
-                ],
-              }}
-              width={Dimensions.get("window").width} // from react-native
-              height={220}
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundColor: "#e26a00",
-                backgroundGradientFrom: "#2F80EA",
-                backgroundGradientTo: "#2F80EA",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16,
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#2F80EA",
-                },
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16,
-              }}
-            />
+            <ExampleGraph />
           </View>
-          <View style={styles.graph}>
+          <View style={styles.container}>
             <Text>Mindset</Text>
-            <LineChart
-              data={{
-                labels: ["9/1", "9/2", "9/3", "9/4", "9/5", "9/6"],
-                datasets: [
-                  {
-                    data: [
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                    ],
-                  },
-                ],
-              }}
-              width={Dimensions.get("window").width} // from react-native
-              height={220}
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundColor: "#e26a00",
-                backgroundGradientFrom: "#2F80EA",
-                backgroundGradientTo: "#2F80EA",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                  borderRadius: 16,
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#2F80EA",
-                },
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16,
-              }}
-            />
+            <ExampleGraph />
+          </View>
+          <View style={styles.container}>
+            <TableExample />
           </View>
         </ScrollView>
-        <View style={styles.table}>
-          <TableExample />
-        </View>
       </SafeAreaView>
     </View>
   );
