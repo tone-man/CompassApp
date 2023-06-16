79af7ffc002f5b73b03fdc8bd16dab21c3231e1d
toni_craveiro <theToneMan@outlook.com>
2023-06-16 14:00:54 -0400

    Made Responses an Enum, Throwing Errors Hopefully
    Finished the POST and PATCH calls, now all thats left is to debug via postman.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const Responses = require("./statusMessages");
+
 /* Opens Database Connection */
 const db = new sqlite3.Database("database/CompassDatabase.db");
 
@@ -13,11 +15,11 @@ app.get("/api/users/:email", (req, res) => {
   db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send(Responses[500]);
     } else if (row) {
       res.json(row);
     } else {
-      res.status(404).send("A user registered with that email was not found.");
+      res.status(404).send(Responses[404]);
     }
   });
 });
@@ -32,11 +34,11 @@ app.get("/api/user_roles/:user_id", (req, res) => {
     (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Responses[500]);
       } else if (row) {
         res.json(row);
       } else {
-        res.status(404).send("The user specified does not have a role.");
+        res.status(404).send(Responses[404]);
       }
     }
   );
@@ -47,11 +49,11 @@ app.get("/api/skills/", (req, res) => {
   db.all("SELECT * FROM skills", (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send(Responses[500]);
     } else if (row) {
       res.json(row);
     } else {
-      res.status(404).send("Skill Types Not Found");
+      res.status(404).send(Responses[404]);
     }
   });
 });
@@ -66,7 +68,7 @@ app.get("/api/skill_mastery/:user_id", (req, res) => {
     (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Responses[500]);
       } else if (row) {
         res.json(row);
       } else {
@@ -83,13 +85,11 @@ app.get("/api/students/:user_id", (req, res) => {
   db.get("SELECT * FROM students WHERE user_id = ?", userId, (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send(Responses[500]);
     } else if (row) {
       res.json(row);
     } else {
-      res
-        .status(404)
-        .send("Student Information not found for the specified user");
+      res.status(404).send(Responses[404]);
     }
   });
 });
@@ -104,11 +104,11 @@ app.get("/api/study_hours/:user_id", (req, res) => {
     (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Responses[500]);
       } else if (row) {
         res.json(row);
       } else {
-        res.status(404).send("Study Hours not found for the specified user");
+        res.status(404).send(Responses[404]);
       }
     }
   );
@@ -124,13 +124,11 @@ app.get("/api/behavior_events/:user_id", (req, res) => {
     (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Responses[500]);
       } else if (row) {
         res.json(row);
       } else {
-        res
-          .status(404)
-          .send("Behavior events not found for the specified user");
+        res.status(404).send(Responses[404]);
       }
     }
   );
@@ -141,11 +139,11 @@ app.get("/api/behaviors", (req, res) => {
   db.all("SELECT * FROM student_behaviors", (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send(Responses[500]);
     } else if (row) {
       res.json(row);
     } else {
-      res.status(404).send("Behaviors not found.");
+      res.status(404).send(Responses[404]);
     }
   });
 });
@@ -160,26 +158,26 @@ app.get("/api/behavior_consequences/:behavior_id", (req, res) => {
     (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Responses[500]);
       } else if (row) {
         res.json(row);
       } else {
-        res.status(404).send("Behaviors not found.");
+        res.status(404).send(Responses[404]);
       }
     }
   );
 });
 
 const insertBehaviorQuery = `INSERT INTO student_behavior_log (user_id, behavior_id, date_of_event)
-VALUES ($userId, $behaviorId, $dateOfEvent)`;
+VALUES ($userId, $behaviorId, $dateOfEvent);`;
 
 function insertBehavior(params) {
   /* VALIDITY CHECK */
 
   db.run(insertBehaviorQuery, params, (err) => {
     if (err) {
-      console.error(err.message);
-      throw new Error();
+      console.error(err);
+      throw new Error("Database Rejected Query");
     } else {
       console.log(
         `Behavior Event Logged As: ${
@@ -203,15 +201,16 @@ app.post("api/behavior_events", (req, res) => {
   try {
     insertBehavior(params);
   } catch (error) {
+    /* TODO: Response for 404 */
     console.error(error);
-    res.status(500).message("Internal Server Error");
+    res.status(500).send(Responses[500]);
   }
 
   try {
     updateStudentStudyHoursRemaining(userId);
   } catch (error) {
     console.error(error);
-    res.status(500).message("Internal Server Error");
+    res.status(500).send(Responses[500]);
     rollbackUpdateToBehaviorLog(params);
   }
 });
@@ -224,8 +223,8 @@ function insertSkillMastery(params) {
 
   db.run(insertSkillMasteryQuery, params, (err) => {
     if (err) {
-      console.error(err.message);
-      throw new Error();
+      console.error(err);
+      throw new Error("Database Reject Query");
     } else {
       console.log(
         `Mastery Skill Logged As: ${
@@ -251,7 +250,7 @@ app.post("api/skill_mastery", (req, res) => {
     insertSkillMastery(params);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).send(Responses[500]);
   }
 });
 
@@ -263,8 +262,8 @@ function insertStudyHours(params) {
 
   db.run(insertStudyHoursQuery, params, (err) => {
     if (err) {
-      console.error(err.message);
-      throw new Error();
+      console.error(err);
+      throw new Error("Database Rejected Query");
     } else {
       console.log(
         `Study Hours Logged As: ${
@@ -288,69 +287,80 @@ app.post("api/study_hours", (req, res) => {
     insertStudyHours(params);
   } catch (error) {
     console.err(error);
-    res.status(500).message("Internal Server Error");
-  }
-
-  try {
-    updateStudentStudyHoursCompleted(params);
-  } catch {
-    console.err(error);
-    RollbackUpdateToStudentStudyLog();
-    res.status(500).message("Internal Server Error");
+    res.status(500).send(Responses[500]);
   }
 });
 
-/* UPDATE BEHAVIOR LOG */
+/* Update Study Hours for a Specific Student */
+
+const updateStudyHoursLogQuery = `UPDATE student_study_hours
+SET log_out_time = $logOutTime AND study_duration = $studyDuration,
+WHERE user_id = $userId AND log_in_time = $logInTime AND dateOfEvent = $dateOfEvent`;
+
+function updateStudyHoursLog(params) {
+  const { $logInTime, $logOutTime } = params;
+
+  params.$studyDuration = $logInTime - $logOutTime;
+
+  db.run(updateStudyHoursLogQuery, params, function (error) {
+    if (error) {
+      console.error(error);
+      throw new Error("Database Rejected Query");
+    } else {
+      console.log(`Log out time updated successfully for user ${userId}`);
+    }
+  });
+}
+
 app.patch("api/study_hours/:user_id", (req, res) => {
   const userId = req.params.user_id;
   const { dateOfEvent, logInTime, logOutTime } = req.body;
 
-  /* TODO: Inpit Validation */
-
-  const studyDuration = logInTime - logOutTime;
+  /* TODO: Input Validation */
 
   const params = {
     $userId: userId,
     $logInTime: logInTime,
     $dateOfEvent: dateOfEvent,
     $logOutTime: logOutTime,
-    $studyDuration: studyDuration,
   };
 
-  db.run(
-    `UPDATE student_study_hours
-    SET log_out_time = $logOutTime AND study_duration = $studyDuration,
-    WHERE user_id = $userId AND log_in_time = $logInTime AND dateOfEvent = $dateOfEvent`,
-    params,
-    function (error) {
-      if (error) {
-        console.error(error.message);
-        res.status(500).send(`Internal Server Error.`);
-      } else {
-        console.log(`Log out time updated successfully for user ${userId}`);
-      }
-    }
-  );
+  try {
+    updateStudyHoursLog(params);
+  } catch (error) {
+    console.err(error);
+    res.status(500).send(Responses[500]);
+  }
+
+  try {
+    updateStudentStudyHoursCompleted(params);
+  } catch (error) {
+    console.err(error);
+    RollbackUpdateToStudentStudyLog();
+    res.status(500).send(Responses[500]);
+  }
 });
 
-/* Sub Query For Updating Student Study Hours */
+/* Sub Query For Updating Student Study Hours Completed*/
 
-const sumStudentStudyTime = `SELECT SUM(study_duration)
+const sumStudentStudyTimeQuery = `SELECT SUM(study_duration)
+  AS sumStudentStudyTime
   FROM student_study_log
   WHERE user_id = ?;`;
 
-const updateStudentHoursCompletedQuery = `UPDATE students
+const updateStudentMinutesCompletedQuery = `UPDATE students
   SET study_minutes_completed = $studyMinutesCompleted
   WHERE user_id = $userId;`;
 
-function updateStudentStudyMinutesCompleted(userId) {
+function updateStudentStudyHoursCompleted(userId) {
   let sumStudyMinutes = null;
 
-  db.get(sumStudentStudyTime, userId, (err, row) => {
+  db.get(sumStudentStudyTimeQuery, userId, (err, row) => {
     if (err) {
       console.log(err);
+      throw new Error("Database Rejected Query.");
     } else if (row) {
-      sumStudyMinutes = row;
+      sumStudyMinutes = row.sumStudentStudyTime;
     }
   });
 
@@ -362,32 +372,53 @@ function updateStudentStudyMinutesCompleted(userId) {
     $studyMinutesCompleted: sumStudyMinutes,
   };
 
-  db.run(updateStudentHoursCompletedQuery, params, (err) => {
+  db.run(updateStudentMinutesCompletedQuery, params, (err) => {
     if (err) {
       console.log(err);
-      throw new Error("Failed to update study hours completed.");
+      throw new Error("Database Rejected Query.");
     } else {
       console.log(`Study hours updated successfully for user ${userId}`);
     }
   });
 }
 
-/*Update for additional study hours*/
-const sumStudentRequiredStudyTime = `SELECT SUM(additional_study_minutes) FROM student_behavior_log
+/* Sub Query For Updating Student Study Hours Required*/
+
+const baseStudentStudyTimeQuery = `SELECT (student_base_minutes) 
+  FROM students
+  WHERE user_id = ?;`;
+
+const sumStudentRequiredStudyTime = `SELECT SUM(additional_study_minutes) 
+  AS sumBehaviorMinutes
+  FROM student_behavior_log
   JOIN student_behavior_consequences
   ON student_behavior_log.behavior_id=student_behavior_consequences.behavior_id
   WHERE user_id = ?;`;
 
-const updateStudentHoursRemainingQuery = `UPDATE students
+const updateStudentMinutesRemainingQuery = `UPDATE students
   SET study_minutes_required = $sumBehaviorMinutes
   WHERE user_id = $userId`;
 
 function updateStudentStudyHoursRemaining(userId) {
-  let sumBehaviorMinutes = null;
+  let baseStudentMinutes,
+    sumBehaviorMinutes = null;
+
+  db.get(baseStudentStudyTimeQuery, userId, (err, row) => {
+    if (err) {
+      console.log(err);
+      throw new Error("Database Rejected Query.");
+    } else if (row) {
+      baseStudentMinutes = row.sumBehaviorMinutes;
+    }
+  });
+
+  if (!baseStudentMinutes)
+    throw ReferenceError("No Base Study Hours Found for User");
 
   db.get(sumStudentRequiredStudyTime, userId, (err, row) => {
     if (err) {
       console.log(err);
+      throw new Error("Database Rejected Query.");
     } else if (row) {
       sumBehaviorMinutes = row;
     }
@@ -395,15 +426,17 @@ function updateStudentStudyHoursRemaining(userId) {
 
   if (!sumBehaviorMinutes) throw ReferenceError("No Behaviors Found for User");
 
+  sumBehaviorMinutes += baseStudentMinutes;
+
   const params = {
     $userId: userId,
     $sumBehaviorMinutes: sumBehaviorMinutes,
   };
 
-  db.run(updateStudentHoursRemainingQuery, params, (err) => {
+  db.run(updateStudentMinutesRemainingQuery, params, (err) => {
     if (err) {
       console.log(err);
-      throw new Error("Failed to update study hours remaining.");
+      throw new Error("Database Rejected Query.");
     } else {
       console.log(
         `Study hours remaining updated successfully for user ${userId}`
@@ -419,6 +452,8 @@ const rollbackUpdateToStudentStudyLogQuery = `UPDATE student_study_hours
   WHERE user_id = $userId AND log_in_time = $logInTime AND dateOfEvent = $dateOfEvent`;
 
 function RollbackUpdateToStudentStudyLog(params) {
+  //TODO: Input validation
+
   db.run(rollbackUpdateToStudentStudyLogQuery, params, (err) => {
     if (err) {
       console.error("Failed to Rollback, will try again.");
@@ -431,6 +466,8 @@ const rollbackUpdateToBehaviorLogQuery = `DELETE FROM student_behavior_log
  WHERE user_id = $userId AND behavior_id = $behaviorId AND date_of_event = $dateOfEvent;`;
 
 function rollbackUpdateToBehaviorLog(params) {
+  //TODO: Input Validation
+
   db.run(rollbackUpdateToBehaviorLogQuery, params, (err) => {
     if (err) {
       console.error("Failed to Rollback, will try again.");
---
+++ backend/statusMessages.js
@@ -0,0 +1,7 @@
+const Responses = {
+  200: "Successfuly Completed Request",
+  404: "Resource failed to be located",
+  500: "Internal Server Error",
+};
+
+module.exports = Responses;
